b0a7e2a79ce35876abfa714be2563b7935d249a3
brotherzhafif <bangz1504@gmail.com>
2024-12-14 02:28:01 +0700

    Adding user and topic relation when question select by id

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -21,7 +21,7 @@ class QuestionController extends Controller
     // Get a single question
     public function show($id)
     {
-        $question = Question::with(['answers.user'])->find($id);
+        $question = Question::with(['user','topic','answers.user'])->find($id);
         if (!$question) {
             return response()->json(['error' => 'Question not found'], 404);
         }
